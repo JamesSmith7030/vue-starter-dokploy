402df55e8f4bd7913b22127efa93ea7a4043a03a
guest0 <guest0@clogic.xyz>
2024-05-13 03:24:20 +0000

    test: Coder 测试代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
   <h2>我是 Webhook 测试代码</h2>
+  <h3>Coder 测试代码</h3>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
